120f09783c0813de1072a82c8f849867b890816c
Merl <menz123@me.com>
2026-03-12 08:54:40 +0100

    chore, upgrade neofs-sdk-ts-core dep

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,13 @@
 export { Decimal } from './decimal';
 export * from './user';
 
+// Re-export enums and helpers from core
+export { MatchType, ChecksumType, ObjectType } from '@axlabs/neofs-sdk-ts-core';
+export { toContainerId, decodeAttributes, isRetryableNeoFSError } from '@axlabs/neofs-sdk-ts-core';
+export { payloadChecksums } from '@axlabs/neofs-sdk-ts-core';
+export { createSigner } from '@axlabs/neofs-sdk-ts-core';
+export { base58Decode, base58Encode } from '@axlabs/neofs-sdk-ts-core';
+
 export interface ContainerID {
   value: Uint8Array;
 }
@@ -16,7 +23,7 @@ export interface Address {
 
 export interface ObjectGetResult {
   objectId: ObjectID;
-  header: any; // Object header
-  signature: any; // Object signature
+  header: any;
+  signature: any;
   payload: Uint8Array;
 }
